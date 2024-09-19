f4c4a17b685c7be033174cb0c843702e976531db
serhii <ockar2018@gmail.com>
2024-09-19 23:03:47 +0300

    change root directory name to task_core

--- task_core/asgi.py
+++ task_core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for task_manager project.
+ASGI config for task_core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_core.settings')
 
 application = get_asgi_application()
--- task_core/wsgi.py
+++ task_core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for task_manager project.
+WSGI config for task_core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_core.settings')
 
 application = get_wsgi_application()
