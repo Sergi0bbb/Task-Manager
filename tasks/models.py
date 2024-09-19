14df4d1013c15fe3941f36bb36ad1bc20ec460f0
serhii <ockar2018@gmail.com>
2024-09-19 23:04:49 +0300

    create models and migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.PROTECT, related_name="workers")
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Task(models.Model):
+    PRIORITY_TYPE_CHOICES = [("L", "Low"), ("M", "Medium"), ("H", "High")]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(choices=PRIORITY_TYPE_CHOICES, max_length=1)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(Worker, related_name="tasks_to_do")
+    giver = models.ForeignKey(Worker, on_delete=models.CASCADE, related_name="given_tasks")
