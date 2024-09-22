7a9881ebc73734ca1a715e9967685e264d483ea8
serhii <ockar2018@gmail.com>
2024-09-23 00:39:06 +0300

    correct views imports

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 
-from tasks.views import (
-    index,
+from tasks.views.calendar import CalendarView
+from tasks.views.index import index
+from tasks.views.tasks import (
+    TaskDetailView,
     create_task,
     complete_task,
-    user_profile,
+    TaskUpdateView,
+    TaskDeleteView
+)
+from tasks.views.worker import (
     WorkersListView,
     WorkersDetailView,
-    AuthorCreateView,
-    CalendarView,
-    TaskDetailView,
+    WorkerCreateView,
     SelectAvatarView,
-    TaskUpdateView,
-    TaskDeleteView,
+    worker_get_update_profile
 )
 
 app_name = "tasks"
@@ -21,14 +23,13 @@ urlpatterns = [
     path("home_page/", index, name="home_page"),
     path("workers/", WorkersListView.as_view(), name="workers-list"),
     path("workers/<int:pk>/", WorkersDetailView.as_view(), name="workers-detail"),
-    path("accounts/sign_up/", AuthorCreateView.as_view(), name="sign_up"),
+    path("accounts/sign_up/", WorkerCreateView.as_view(), name="sign_up"),
     path("calendar/", CalendarView.as_view(), name="calendar"),
-    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-    path('create_task/', create_task, name='task-create'),
-    path('task/<int:pk>/complete/', complete_task, name='complete-task'),
-    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
-    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
-    path('profile/update/', user_profile, name='user-profile'),
-    path('profile/select_avatar/', SelectAvatarView.as_view(), name='select-avatar'),
-
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("create_task/", create_task, name="task-create"),
+    path("task/<int:pk>/complete/", complete_task, name="complete-task"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("profile/update/", worker_get_update_profile, name="user-profile"),
+    path("profile/select_avatar/", SelectAvatarView.as_view(), name="select-avatar"),
 ]
