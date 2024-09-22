043d0e2e6e3923a1fd100276e2941559ef7a02c8
serhii <ockar2018@gmail.com>
2024-09-22 23:59:50 +0300

    override str method for TaskType, Position and Task models. add new filed "avatar" to Worker

--- tasks/models.py
+++ tasks/models.py
@@ -1,17 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class TaskType(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.PROTECT, related_name="workers")
+    avatar = models.CharField(default="avatars/avatar1.png", max_length=100)
 
     def __str__(self):
         return f"{self.username}"
@@ -28,3 +36,11 @@ class Task(models.Model):
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
     assignees = models.ManyToManyField(Worker, related_name="tasks_to_do")
     giver = models.ForeignKey(Worker, on_delete=models.CASCADE, related_name="given_tasks")
+
+    def __str__(self):
+        return f"Name: {self.name}, deadline {self.deadline}"
+
+    @property
+    def get_html_url(self):
+        url = reverse("tasks:task-detail", args=(self.id,))
+        return f'<a href="{url}"> {self.name} </a>'
