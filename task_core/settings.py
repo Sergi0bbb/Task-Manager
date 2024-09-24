ecb4c12c8c06e4e3d571b9ed0ac09c702e19757b
serhii <ockar2018@gmail.com>
2024-09-24 16:27:12 +0300

    deployment configs

--- task_core/settings.py
+++ task_core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"] != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "task-manager-acs0.onrender.com"]
 
 
 # Application definition
